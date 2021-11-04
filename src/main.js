bf5b9fd3d0c95289862d4ace9f56cfce21f0cdde
Spidust <tranhaidangg2@gmail.com>
2021-11-04 13:20:21 +0700

    commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
